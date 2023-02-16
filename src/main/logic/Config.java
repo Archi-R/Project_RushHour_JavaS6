809b399ca5b929b9b2ee68b9317483cf35b167f6
hypo368 <114984095+hypo368@users.noreply.github.com>
2023-02-16 09:31:26 +0100

    add attributes and methods to config class, comment missing

--- src/main/logic/Config.java
+++ src/main/logic/Config.java
@@ -1,7 +1,42 @@
 package logic;
 
+/**
+ * Class that represent the different configuration.
+ * @author
+ * @version
+ */
 public class Config {
+
     /**
-     * Class that represent the different configuration.
+     * level number of the configuration
      */
+    private int level;
+
+
+    /**
+     * Configuration of the cars on the board, liste of position
+     * of the cars on the board
+     * AV03 représente la voiture de couleur “light_green” placée
+     * verticalement à la position (0,3).
+     */
+    private String[] config;
+
+    // methods
+
+
+    //public String[] parserConfig(String conf) {
+    //    // parse
+    //}
+
+    public void setLevel(int l){ this.level =l; }
+
+
+    public int getLevel() { return level; }
+
+    public void setConfig() { // set config
+    }
+
+    public String[] getConfig(){ return config; }
+
+
 }
