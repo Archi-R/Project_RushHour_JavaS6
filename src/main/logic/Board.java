0ad0edb06e9b652b22a85b279fada4bec08ac7b8
hypo368 <114984095+hypo368@users.noreply.github.com>
2023-02-16 08:11:39 +0100

    add attributes to board class

--- src/main/logic/Board.java
+++ src/main/logic/Board.java
@@ -4,4 +4,25 @@ public class Board {
     /**
      * Class representing the board object and it's function.
      */
+
+    /**
+     * difficulties enumeration
+     */
+    public enum Difficulty {beginner, intermediate, advanced, expert, grandmaster};
+
+    /**
+     * The board size.
+     */
+    final int boardsize = 6;
+
+    /**
+     * Define the difficulty of the current board
+     */
+    Difficulty difficulty;
+
+    /**
+     * Define the configuration of the current board
+     */
+    Config configuration;
+
 }
