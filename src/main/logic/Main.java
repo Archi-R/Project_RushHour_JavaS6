48352ff9746c9721c412e42d025c17f9d65da019
hypo368 <114984095+hypo368@users.noreply.github.com>
2023-02-27 11:16:02 +0100

    add main start

--- src/main/logic/Main.java
+++ src/main/logic/Main.java
@@ -1,7 +1,22 @@
 package logic;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Rush hour game");
+        System.out.println("Choose a difficulty : (beginner,intermediate,advanced,expert,grandmaster)");
+        Difficulty difficulty = Difficulty.beginner; // default difficulty
+        //TODO : wait for user input and add it in difficulty
+
+        System.out.println("Choose a level"); //TODO : return the level number in the selected difficulty for the player
+        int level = 1; //default level
+        //TODO : wait for user input and add it in level
+
+        Config config = new Config(difficulty,level);
+        Board board = new Board(difficulty,config);
+        board.initiate(); // load the configuration
+
     }
 }
